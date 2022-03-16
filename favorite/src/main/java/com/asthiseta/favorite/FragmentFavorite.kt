cf630315e6f289cd4d3a318df70a27a511512226
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-16 08:48:43 +0700

    refactoring removed unused import

--- favorite/src/main/java/com/asthiseta/favorite/FragmentFavorite.kt
+++ favorite/src/main/java/com/asthiseta/favorite/FragmentFavorite.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.FragmentNavigatorExtras
